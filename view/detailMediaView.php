29be34a3aae089d8c00b62e596e7c2e37361478d
Axel Pélerin <arcanynka@gmail.com>
2020-06-23 14:12:54 +0200

    Add raw informations for the detailed media page. Need to be reworked with css.

--- view/detailMediaView.php
+++ view/detailMediaView.php
@@ -1,7 +1,14 @@
 <?php ob_start(); ?>
 
-Hello there
-<?= $media['title']; ?>
+<h3 class="mediaD-title"><?= $media['title']; ?></h3>
+
+<div class="info-media">
+    <div><?= $media['type']?></div>
+    <div><?= $media['status']?></div>
+    <div><?= $media['release_date']?></div>
+    <div><?= $media['summary']?></div>
+    <div><?= $media['trailer_url']?></div>
+</div>
 
 <?php $content = ob_get_clean(); ?>
 
